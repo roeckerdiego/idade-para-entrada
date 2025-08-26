3ad6a7a163122aaf2e4e4c97f5059ddb7e02ffc6
roeckerdiego <diego.bram.oliveira@escola.pr.gov.br>
2025-08-26 11:51:00 +0000

    terminado

--- idade.py
+++ idade.py
@@ -1,11 +1,12 @@
+
 nome = input("Qual seu nome?")
 idade = int(input("Qual sua idade:"))
 
-if 1 > idade < 10:
+if 1< idade <10:
     print("Parabéns, você consegue entrar como criança.")
-elif 11 > idade < 19:
+elif 11< idade <19:
     print("Ok, você ja consegue entrar como adolecente.")
-elif 20 > idade < 59:
+elif 20< idade <59:
     print("Ok, você está classificado como adulto.")
 else:
     print("Parabéns, você entra como idoso.")
